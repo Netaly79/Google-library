8a07260b3d9eb14e25c28876258f9405bf67b770
Karpenko Nataliia <Netaly79@gmail.com>
2019-10-05 18:15:13 +0300

    read more

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 "use strict"
 let $searchForm=$("#search-form");
+let $bookList=$("#book-list");
+let books=[];
+let $currentBook=$('#current-book');
+let $bookDescription=$(".book-description")
 
 $searchForm.on("submit", function(event) {
     event.preventDefault();
@@ -15,8 +19,37 @@ function getBooks(query){
         method:"GET",
         data: `q=${query}`
     }).done((response) => {
-        console.log("response ", response);
+        books=response.items;
+        addBooks(response.items);
     }).fail ((error) => {
         console.log ("error", error);
     })
 }
+function addBooks(data){
+    $bookList.empty();
+    data.forEach((book) => {
+        $("<a href=''>").addClass("list-group-item")
+        .text(book.volumeInfo.title)
+        .attr("data-id", book.id)
+        .appendTo($bookList)
+    });
+}
+$bookList.on("click","[data-id]", function (event){
+    event.preventDefault();
+    let bookId=$(this).data("id");
+    console.log (bookId);
+    let book=books.find((item) =>item.id===bookId);
+    console.log (book);
+    $currentBook.fadeIn();
+    $currentBook.find(".book-title").text(`${book.volumeInfo.title} | ${book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "No author"} ${book.volumeInfo.publishedDate}`);
+    $bookDescription.empty();
+    
+    $("<img>").attr("src",book.volumeInfo.imageLinks.thumbnail).appendTo($bookDescription);
+    $("<a>").attr("href", book.volumeInfo.previewLink)
+    .attr("target","_blank")
+    .text("read more...")
+    .addClass("read-link")
+    .appendTo($bookDescription);
+
+
+})
